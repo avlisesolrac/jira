78f80f4e456a15d4a44a1c96e59ee6ac10ad256a
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2025-11-12 22:05:41 -0300

    start: Desenvolver Box Popup ao Clicar no Botão - DEV-95

--- DEV-94/assets/js/menuFixoComBarraDeRolagem.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function menuFixoComBarraDeRolagem(){
-	if(window.pageYOffset > 0){
-		document.querySelector('nav').classList.add("exibirMenu");
-	}else{
-		document.querySelector('nav').classList.remove("exibirMenu");
-	}
-}
